116d677ba725bb8e8f68883afbf29cc2942b1d11
qiuguoliang <guoliang.qiu@me2zen.com>
2025-06-20 13:57:43 +0800

    对话系统开始节点

--- DialogueDeath/Script/Node/StartNode.cs
+++ DialogueDeath/Script/Node/StartNode.cs
@@ -1,9 +1,11 @@
+using System;
 using GraphProcessor;
 
 
 /// <summary>
 /// 对话流程开始节点
 /// </summary>
+[Serializable, NodeMenuItem("Dialogue/StartNode")]
 public class StartNode : DialogueBaseNode
 {
     public override string name => "对话开始执行";
