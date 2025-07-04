1cf9f9aaee123f3d630dd828d5f9fad54d23fc41
qiuguoliang <guoliang.qiu@me2zen.com>
2025-07-04 19:04:52 +0800

    log 添加error

--- CommonDeath/Debugger.cs
+++ CommonDeath/Debugger.cs
@@ -2,6 +2,16 @@ using UnityEngine;
 
 public static class Debuggers
 {
+    
+    public static void Error(string logTag, object message)
+    {
+        Error(logTag, message, (Object)null);
+    }
+
+    public static void Error(string logTag, object message, Object context)
+    {
+        Debug.LogError((object)string.Format("[{0}]: {1}", (object)logTag, message), context);
+    }
     public static void Log(string logTag, object message)
     {
         Log(logTag, message, (Object)null);
